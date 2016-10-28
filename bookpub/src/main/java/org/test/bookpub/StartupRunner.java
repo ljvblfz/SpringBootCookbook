c0d58538d5ee8c1cc69adaf3b735b912136b1db0
ljvblfz <ljvblfz@sina.com>
2016-10-29 05:09:38 +0800

    Format code and remove redundant whitespace.

--- bookpub/src/main/java/org/test/bookpub/StartupRunner.java
+++ bookpub/src/main/java/org/test/bookpub/StartupRunner.java
@@ -6,7 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 
 public class StartupRunner implements CommandLineRunner {
     protected final Log logger = LogFactory.getLog(getClass());
-    
+
     @Override
     public void run(String... args) throws Exception {
         logger.info("Hello");
