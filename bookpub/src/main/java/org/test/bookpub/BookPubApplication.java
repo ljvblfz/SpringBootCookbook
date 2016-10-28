a39ba5f618154537973581b650b0cc68eb025de4
ljvblfz <ljvblfz@sina.com>
2016-10-29 04:51:07 +0800

    Define StartupRunner class as @Bean in the BookPubApplication.java application configuration.

--- bookpub/src/main/java/org/test/bookpub/BookPubApplication.java
+++ bookpub/src/main/java/org/test/bookpub/BookPubApplication.java
@@ -2,9 +2,15 @@ package org.test.bookpub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookPubApplication {
+
+	@Bean
+	public StartupRunner schedulerRunner() {
+		return new StartupRunner();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookPubApplication.class, args);
